ccce3c0eba269ce1c123b0d387d5ea0817c5d282
Hamidul-1 <hamidul786cse@gmail.com>
2022-10-01 01:02:52 +0600

    first commit

--- src/Pages/Shared/Navbar2.js
+++ src/Pages/Shared/Navbar2.js
@@ -3,7 +3,14 @@ import React from 'react';
 const Navbar2 = () => {
     return (
         <div class="navbar bg-neutral text-neutral-content">
-            <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+            <a class="btn btn-ghost normal-case text-xl">All Categories</a>
+            <a class="btn btn-ghost normal-case text-xl">Home</a>
+            <a class="btn btn-ghost normal-case text-xl">Offers</a>
+            <a class="btn btn-ghost normal-case text-xl">Marketing Tools</a>
+            <a class="btn btn-ghost normal-case text-xl">Tag</a>
+            <a class="btn btn-ghost normal-case text-xl">News Feed</a>
+            <a class="btn btn-ghost normal-case text-xl">Review</a>
+            <a class="btn btn-ghost normal-case text-xl">FEQ</a>
         </div>
     );
 };
