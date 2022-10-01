ca487cdc6c6f89ebf04340c93c843c6f3bcc8b82
Hamidul-1 <hamidul786cse@gmail.com>
2022-10-01 17:50:52 +0600

    first commit

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div class="hero min-h-screen bg-base-200">
+        <div class="hero min-h-screen ">
             <div class="hero-content flex-col lg:flex-row-reverse">
                 <img src="https://placeimg.com/260/400/arch" class="max-w-sm rounded-lg shadow-2xl" />
                 <div>
